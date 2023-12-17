f85aff86bd0ebecf1a65b4e0d03d0a80ab70d772
unknown <gourav052003@gmail.com>
2023-12-17 21:01:24 +0530

    template + requirements added

---
+++ template.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+FILENAMES = [
+
+        Path(f".github/workflows/main.yaml"),
+        Path(f"dvc.yaml"),
+
+        Path(f"Artifacts"),
+
+        Path(f"Src/__init__.py"),
+
+        Path(f"Src/Research/experiments.ipynb"),
+        
+        Path(f"Src/Entities/__init__.py"),
+        Path(f"Src/Entities/entity.py"),
+
+        Path(f"Src/Logger.py"),
+        Path(f"Src/Utils.py"),
+
+        Path(f"Src/Config.yaml"),
+        Path(f"Src/Params.yaml"),
+
+        Path(f"Src/Configuration/__init__.py"),
+        Path(f"Src/Configuration/config.py"),
+        
+        Path(f"Src/Constant.py"),
+
+        Path(f"Src/Components/__init__.py"),
+        Path(f"Src/Pipelines/__init__.py"),
+        Path(f"Src/Main.py"),
+
+        Path(f"Application")
+
+]
+
+
+
+
+
